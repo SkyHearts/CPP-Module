a2423bde7db7ecf2559326dcc6207736d3138fa9
Jiun Jye Yim <jyim@u90z02s01.42kl.edu.my>
2023-07-12 14:44:32 +0800

    Pointer and reference

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 12:50:33 by jyim              #+#    #+#             */
+/*   Updated: 2023/07/12 13:07:07 by jyim             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+//to test type&
+void edit_string1(std::string str){
+	str += " Bye Bye";
+}
+
+int main(){
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "Memory address" << std::endl;
+	std::cout << "string variable: " << &str << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	//edit_string1(str);
+
+	std::cout << "Value" << std::endl;
+	std::cout << "string variable: " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+	return (0);
+}
