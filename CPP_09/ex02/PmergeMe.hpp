62e85a6f96be861dc9db527e20b2605075f3f9bb
Jiun Jye Yim <jiunjye.yim@gmail.com>
2023-11-22 17:06:45 +0800

    Fix: To only consider lower bound

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:49:06 by jyim              #+#    #+#             */
-/*   Updated: 2023/11/15 10:29:37 by jyim             ###   ########.fr       */
+/*   Updated: 2023/11/18 16:13:56 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,41 +38,67 @@ class PmergeMe
 		void PrintDeque(void);
 		void PrintVector(void);
 		template<typename Container>
-		void MergeSort(Container& array){
-			int length = array.size();
-				if (length <= 1) return;
-			int middle = length * 0.5;
-			Container left(array.begin(), array.begin() + middle);
-			Container right(array.begin() + middle, array.end());
-			MergeSort(left);
-			MergeSort(right);
-			Merge(left, right, array);
+		void InsertionSort(Container& array, int left, int right){
+			for (int i = left + 1; i <= right; i++)
+			{
+				int tmp = array[i];
+				int j = i - 1;
+				while ((j >= left) && (array[j] > tmp)) {
+					array[j + 1] = array[j];
+					j--;
+				}
+				array[j + 1] = tmp;
+			}
 		}
 		template<typename Container>
-		void Merge(Container& left, Container& right, Container& results){
-			int L_size = left.size();
-			int R_size = right.size();
-			int i = 0, j = 0, k = 0;
+		void MergeSort(Container& array, int left, int right, int size){
+			// int length = array.size();
+			// if (length <= 1)
+			// 	return;
+			// if (left < right){
+			if (right - left <= size - 1){
+				InsertionSort(array, left, right);
+			}
+			else{
+				int middle = left + (right - left) * 0.5;
+				// Container LeftArray(array.begin(), array.begin() + middle);
+				// Container RightArray(array.begin() + middle, array.end());
+				MergeSort(array, left, middle, size);
+				MergeSort(array, middle + 1, right, size);
+				Merge(array, left, right, middle);	
+			}
+			// }
+		}
+		template<typename Container>
+		void Merge(Container& array, int left, int right, int middle){
+			int i = 0, j = 0, k = left;
+
+			int s1 = (middle - left) + 1;
+			int s2 = (right - middle);
 			
-			while (j < L_size && k < R_size) 
-			{
-				if (left[j] <= right[k]) {
-					results[i] = left[j];
-					j++;
+			Container LeftArray(s1);
+			Container RightArray(s2);
+
+			for (i = 0; i < s1; ++i)
+				LeftArray[i] = array[left + i];
+			for (j = 0; j < s2; ++j)
+				RightArray[j] = array[middle + 1 + j];
+
+			i = 0, j = 0;
+			
+			while (i < s1 && j < s2) {
+				if (LeftArray[i] <= RightArray[j]) {
+					array[k++] = LeftArray[i++];
 				}
 				else {
-					results[i] = right[k];
-					k++;
+					array[k++] = RightArray[j++];
 				}
-				i++;
 			}
-			while (j < L_size) {
-				results[i] = left[j];
-				j++; i++;
+			while (i < s1) {
+				array[k++] = LeftArray[i++];
 			}
-			while (k < R_size) {
-				results[i] = right[k];
-				k++; i++;
+			while (j < s2) {
+				array[k++] = RightArray[j++];
 			}
 		}
 		std::vector<int>& getVector(void) { return _vector; }
--- CPP_09/ex02/main.cpp
+++ CPP_09/ex02/main.cpp
@@ -6,18 +6,21 @@
 /*   By: jyim <jyim@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:46:09 by jyim              #+#    #+#             */
-/*   Updated: 2023/11/15 10:09:54 by jyim             ###   ########.fr       */
+/*   Updated: 2023/11/18 20:19:24 by jyim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
-#include <sys/time.h>
+// #include <sys/time.h>
+#include <ctime>
+// #include <unistd.h>
 
 double processTime(ts start, ts end){
 	double time_taken;
     time_taken = (end.tv_sec - start.tv_sec) * 1e6;
 	// std::cout << time_taken << std::endl;
     time_taken = (time_taken + (end.tv_nsec - start.tv_nsec) * 1e-3);
+	// std::cout << (end.tv_nsec - start.tv_nsec) << std::endl;
 	// std::cout << time_taken << std::endl;
  
     // cout << "Time taken by program is : " << fixed
@@ -36,27 +39,36 @@ int main(int argc, char **argv)
 	{
 		PmergeMe merge;
 		ts start, end;
+		// start.tv_nsec = 0; start.tv_sec = 0;
+		// end.tv_nsec = 0; end.tv_sec = 0;
 		merge.ParseNumbers(++argv);
 		std::cout << "Before: ";
 		merge.PrintVector();
 		std::ios_base::sync_with_stdio(false);
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		merge.MergeSort(merge.getVector());
-		clock_gettime(CLOCK_MONOTONIC, &end);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+		merge.MergeSort(merge.getVector(), 0, merge.getVector().size() - 1, 10);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 		std::cout << "After: ";
 		merge.PrintVector();
-		std::cout << "Time to process a range of " << merge.getVector().size() << " elements with std::vector<int> : " << std::fixed << processTime(start, end) << std::setprecision(5) << " us" << std::endl;
-		// std::cout << "Before deque: ";
-		// merge.PrintDeque();
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		merge.MergeSort(merge.getDeque());
-		clock_gettime(CLOCK_MONOTONIC, &end);
+		if (merge.CheckDuplicateVector())
+			std::cout << "error: merge sort failed" << std::endl;
+		// start.tv_nsec = 0; start.tv_sec = 0;
+		// end.tv_nsec = 0; end.tv_sec = 0;
+		std::cout << "Time to process a range of " << merge.getVector().size() << " elements with std::vector<int> : " << std::fixed << std::setprecision(3) << processTime(start, end) <<  " us" << std::endl;
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+		merge.MergeSort(merge.getDeque(), 0, merge.getDeque().size() - 1, 10);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
 		std::cout << "Time to process a range of " << merge.getDeque().size() << " elements with std::deque<int> : " << processTime(start, end) << " us" << std::endl;
-		// std::cout << "After deque: ";
-		// merge.PrintDeque();
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 }
+
+// Time to process a range of  3000 elements with std::vector:   5.2254 ms; cmp: 30546
+// Time to process a range of  3000 elements with  std::deque:   7.5885 ms; cmp: 30546
+// Time to process a range of  3000 elements with std::vector:   2.8652 ms; cmp: 30425
+// Time to process a range of  3000 elements with  std::deque:  10.2546 ms; cmp: 30425
+// Time to process a range of  3000 elements with std::vector:   3.8632 ms; cmp: 30435
+// Time to process a range of  3000 elements with  std::deque:   7.3743 ms; cmp: 30435
